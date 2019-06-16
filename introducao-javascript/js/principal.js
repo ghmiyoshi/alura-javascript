2aa774792c2264915ecf88376e30d53e81e51b2b
ghmiyoshi <gabriel_hidekii@hotmail.com>
2019-06-16 13:06:45 -0300

    Adicionando pacientes na tabela

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -43,17 +43,46 @@ for(var i = 0; i < pacientes.length; i++){
 
     console.log(imc);
 
-    var botaoAdicionar = document.querySelector("#adicionar-paciente");
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
     botaoAdicionar.addEventListener("click", function(event){
         event.preventDefault();
         console.log("O botão foi clicado");
+
+
+        var form = document.querySelector("#form-adiciona");
+
+        var nome = form.nome.value;
+        var peso = form.peso.value;
+        var altura = form.altura.value;
+        var gordura = form.gordura.value;
+
+        var pacienteTr = document.createElement("tr");
+        var nomeTd = document.createElement("td");
+        var pesoTd = document.createElement("td");
+        var alturaTd = document.createElement("td");
+        var gorduraTd = document.createElement("td");
+
+        nomeTd.textContent = nome;
+        pesoTd.textContent = peso;
+        alturaTd.textContent = altura;
+        gorduraTd.textContent = gordura;
+
+        pacienteTr.appendChild(nomeTd);
+        pacienteTr.appendChild(pesoTd);
+        pacienteTr.appendChild(alturaTd);
+        pacienteTr.appendChild(gorduraTd);
+
+        var tabela = document.querySelector("#tabela-pacientes");
+        tabela.appendChild(pacienteTr);
+
     });
 
     titulo.addEventListener("click", function(event){
         console.log("Fui clicado");
     });
 
-}
 
 
 
