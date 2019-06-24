65a0311c0acd8c61708256c76428dbfe25bf220e
ghmiyoshi <gabriel_hidekii@hotmail.com>
2019-06-23 23:59:37 -0300

    Filtrando com expressão regular

--- introducao-javascript/js/filtrar-paciente.js
+++ introducao-javascript/js/filtrar-paciente.js
@@ -3,14 +3,29 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 campoFiltro.addEventListener("input", function(){
     console.log(this.value);
 
-    var pacientes = querySelectorAll(".pacientes");
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if(this.value.length > 0){
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
     
-    for(var i = 0; pacientes.length; i++){
-        var paciente = pacientes[i];
+            var pacienteTdNome = paciente.querySelector(".info-nome");
+            var nome = pacienteTdNome.textContent;
+            var expressao = new RegExp(this.value, "i"); // Busca de texto: expressão regular
+                                                        // this.value - termo digitado no campo de busca
+                                                        // i - indica a opção case insensitive:
+            if(!expressao.test(nome)){
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }else {
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+    
+            paciente.classList.remove("invisivel");
+        }
 
-        var pacienteTdNome = paciente.querySelector(".info-nome");
-        var nome = paciente.textContent;
-
-    }
-
+    } 
 });
